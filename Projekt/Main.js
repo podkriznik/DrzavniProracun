e8cd7a7356e4ead68a875d0a8294f7d87db036e1
podkriznik <48052891+podkriznik@users.noreply.github.com>
2020-01-08 12:12:12 +0100

    Add files via upload

--- Projekt/Main.js
+++ Projekt/Main.js
@@ -1,7 +1,7 @@
 // get data from elastic search
 async function getData () {
     // await response of fetch call
-    let response = await fetch('http://localhost:9200/proracun_skupaj/_search?size=1600&filter_path=hits.hits._source');
+    let response = await fetch('http://localhost:9200/proracun_skupaj/_search?sort=stevilka:asc&size=1600&filter_path=hits.hits._source');
     // only proceed once promise is resolved
     let data = await response.json();
     // only proceed once second promise is resolved
@@ -42,15 +42,17 @@ function izrisHistogram(){
         var materialChart = new google.visualization.LineChart(document.getElementById('histogram'));
 
         function selectHandler() {        
-            leto = graphData.getValue(materialChart.getSelection()[0].row, 0);
-            
             //Nastavi vrsto podatkov za naslednji graf
-            var selectedItem = materialChart.getSelection()[0];
-            if(graphData.getValue(selectedItem.row, selectedItem.column) == graphData.getValue(materialChart.getSelection()[0].row, 1))
-                stevilka = 70;
-            else if(graphData.getValue(selectedItem.row, selectedItem.column) == graphData.getValue(materialChart.getSelection()[0].row, 2))
-                stevilka = 40;
-            else stevilka = 0;
+            var selectedItem = materialChart.getSelection();
+            if (selectedItem.length > 0){
+                leto = graphData.getValue(materialChart.getSelection()[0].row, 0);
+                if(graphData.getValue(selectedItem[0].row, selectedItem[0].column) == graphData.getValue(materialChart.getSelection()[0].row, 1))
+                    tip1 = 70;
+                else if(graphData.getValue(selectedItem[0].row, selectedItem[0].column) == graphData.getValue(materialChart.getSelection()[0].row, 2))
+                    tip1 = 40;
+                else tip1 = 0;
+            }
+            else tip1 = 0;
 
 
             //pripravi podatke
@@ -59,17 +61,17 @@ function izrisHistogram(){
                     //Spraznimo, ker so notri še podatki od prej
                     prihodki = [];
                     odhodki = [];
-                    if(stevilka == 70){
+                    if(tip1 == 70){
                         for (let i = 0; i < data["hits"]["hits"].length; i++) {
                             if(data["hits"]["hits"][i]["_source"]["stevilka"] >= 70 && data["hits"]["hits"][i]["_source"]["stevilka"] < 80){
-                                prihodki.push([data["hits"]["hits"][i]["_source"]["tip"], data["hits"]["hits"][i]["_source"][leto.toString()]])
+                                prihodki.push([data["hits"]["hits"][i]["_source"]["tip"], data["hits"]["hits"][i]["_source"][leto.toString()], data["hits"]["hits"][i]["_source"]["stevilka"]])
                             }
                         }
                     }
-                    else if (stevilka == 40){
+                    else if (tip1 == 40){
                         for (let i = 0; i < data["hits"]["hits"].length; i++) {
                             if(data["hits"]["hits"][i]["_source"]["stevilka"] >= 40 && data["hits"]["hits"][i]["_source"]["stevilka"] < 50){
-                                odhodki.push([data["hits"]["hits"][i]["_source"]["tip"], data["hits"]["hits"][i]["_source"][leto.toString()]])
+                                odhodki.push([data["hits"]["hits"][i]["_source"]["tip"], data["hits"]["hits"][i]["_source"][leto.toString()], data["hits"]["hits"][i]["_source"]["stevilka"]])
                             }
                         }
                     }
@@ -77,7 +79,6 @@ function izrisHistogram(){
                 .then(() => {
                     izrisDonut()
                 })
-
             }
 
         // Listen for the 'select' event, and call my function selectHandler() when
@@ -94,20 +95,28 @@ function izrisDonut(){
 
         var graphData = new google.visualization.DataTable();
         
-        if (stevilka == 70){
+        if (tip1 == 70){
+            document.getElementById('outerTreemap').hidden = true;
+            document.getElementById('naslovTreemap').innerHTML = "";
+            document.getElementById('ponastaviButton').style.visibility = 'hidden';
             document.getElementById('donut').hidden = false;
             document.getElementById('naslovDonut').innerHTML = "Prihodki za leto " + leto;
             graphData.addColumn('string', 'Vrsta prihodka')
             graphData.addColumn('number', 'Vsota')
+            graphData.addColumn('number', 'Tip')
             for (let i = 0; i < prihodki.length; i++) {
                 graphData.addRow(prihodki[i]); 
             }
         }
-        else if (stevilka == 40){
+        else if (tip1 == 40){
+            document.getElementById('outerTreemap').hidden = true;
+            document.getElementById('naslovTreemap').innerHTML = "";
+            document.getElementById('ponastaviButton').style.visibility = 'hidden';
             document.getElementById('donut').hidden = false;
             document.getElementById('naslovDonut').innerHTML = "Odhodki za leto " + leto;
             graphData.addColumn('string', 'Vrsta odhodka')
             graphData.addColumn('number', 'Vsota')
+            graphData.addColumn('number', 'Tip')
             for (let i = 0; i < odhodki.length; i++) {
                 graphData.addRow(odhodki[i]); 
             }
@@ -115,6 +124,9 @@ function izrisDonut(){
         else {
             document.getElementById('donut').hidden = true;
             document.getElementById('naslovDonut').innerHTML = "";
+            document.getElementById('ponastaviButton').style.visibility = 'hidden';
+            document.getElementById('outerTreemap').hidden = true;
+            document.getElementById('naslovTreemap').innerHTML = "";
         }
 
         if (graphData.wg.length){ //Če je velikost tabele večja od 0
@@ -127,22 +139,115 @@ function izrisDonut(){
             width: 1440,
             height: 550,
             colors:['#1a8693', '#751a33', '#d4b95e', '#b34233', '#d28f33', '#4ea2a2', '#885845'],
+            view: {'columns': [0,1]},
         };
 
         var chart = new google.visualization.PieChart(document.getElementById('donut'));
-        if (stevilka){ //Če je številka različna od 0
+        if (tip1){ //Če je številka različna od 0
             chart.draw(graphData, options);
+
+        function selectHandler(){
+            let selection = chart.getSelection()
+            razdeljeno = {};
+            if (selection.length > 0){
+                tip2 = graphData.getValue(selection[0].row, 2)
+
+                getData()
+                .then(data => {
+                    for (let i = 0; i < data["hits"]["hits"].length; i++) {
+                        parent[data["hits"]["hits"][i]["_source"]["stevilka"]] = data["hits"]["hits"][i]["_source"]["tip"];
+                    }
+                    for (let i = 0; i < data["hits"]["hits"].length; i++) {
+                        if(data["hits"]["hits"][i]["_source"][leto.toString()] > 0 && !(data["hits"]["hits"][i]["_source"]["tip"] in razdeljeno)){
+                            if(data["hits"]["hits"][i]["_source"]["stevilka"] == tip2){
+                                razdeljeno[data["hits"]["hits"][i]["_source"]["tip"]] = [data["hits"]["hits"][i]["_source"]["tip"], null, data["hits"]["hits"][i]["_source"][leto.toString()], data["hits"]["hits"][i]["_source"][leto.toString()]];
+                            }
+                            if(data["hits"]["hits"][i]["_source"]["stevilka"] >= tip2*10 && data["hits"]["hits"][i]["_source"]["stevilka"] < tip2*10+10){
+                                razdeljeno[data["hits"]["hits"][i]["_source"]["tip"]] = [data["hits"]["hits"][i]["_source"]["tip"], parent[tip2], data["hits"]["hits"][i]["_source"][leto.toString()], data["hits"]["hits"][i]["_source"][leto.toString()]];
+                            }
+                            if(data["hits"]["hits"][i]["_source"]["stevilka"] >= tip2*100 && data["hits"]["hits"][i]["_source"]["stevilka"] < tip2*100+100){
+                                razdeljeno[data["hits"]["hits"][i]["_source"]["tip"]] = [data["hits"]["hits"][i]["_source"]["tip"], parent[Math.floor(data["hits"]["hits"][i]["_source"]["stevilka"] / 10)], data["hits"]["hits"][i]["_source"][leto.toString()], data["hits"]["hits"][i]["_source"][leto.toString()]];
+                            }
+                            if(data["hits"]["hits"][i]["_source"]["stevilka"] >= tip2*10000 && data["hits"]["hits"][i]["_source"]["stevilka"] < tip2*10000+10000){
+                                razdeljeno[data["hits"]["hits"][i]["_source"]["tip"]] = [data["hits"]["hits"][i]["_source"]["tip"], parent[Math.floor(data["hits"]["hits"][i]["_source"]["stevilka"] / 100)], data["hits"]["hits"][i]["_source"][leto.toString()], data["hits"]["hits"][i]["_source"][leto.toString()]];
+                            }
+                        }
+                    }
+                })
+                .then(() => {
+                    izrisTreemap();
+                })
+            }
+            else{
+                document.getElementById('outerTreemap').hidden = true;
+                document.getElementById('ponastaviButton').style.visibility = 'hidden';
+                document.getElementById('naslovTreemap').innerHTML = "";
+            }
+        }
+        google.visualization.events.addListener(chart, 'select', selectHandler);
         }
     }
-
 }
 
-var leto;
-var stevilka;
-var podatki = [];
-var prihodki = [];
-var odhodki = [];
+function izrisTreemap(){
+    google.charts.load("current", {packages:["treemap"], 'language': 'sl'});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
 
+        document.getElementById('outerTreemap').hidden = false;
+        document.getElementById('naslovTreemap').innerHTML = parent[tip2];
+        document.getElementById('ponastaviButton').style.visibility = 'visible';
+
+        var graphData = new google.visualization.DataTable();
+        graphData.addColumn('string', 'Vrsta');
+        graphData.addColumn('string', 'Parent');
+        graphData.addColumn('number', 'Vsota');
+        graphData.addColumn('number', 'tip');
+
+        for (var key in razdeljeno){
+            graphData.addRow(razdeljeno[key]);
+        }
+
+        tree = new google.visualization.TreeMap(document.getElementById('treemap'));
+
+        var options = {
+            width: 1200,
+            height: 550,
+            fontSize: 15,
+            fontFamily: "Raleway",
+            highlightOnMouseOver: true,
+            maxPostDepth: 3,                    
+            minColor: '#95c8d8',
+            midColor: '#588bae',
+            maxColor: '#0e4d92',
+            headerHeight: 0,
+            generateTooltip: showTooltip,
+        };
+        tree.draw(graphData, options);
+
+        function showTooltip(row, size, value){
+            var formatter = new google.visualization.NumberFormat({pattern: '#,###.00', suffix: '\u20AC'}); //znak za EUR
+            return '<div style="background:white; padding:10px; border-style:solid">' + 
+                   '<span style="font-family:"Raleway"><b>' + graphData.getValue(row, 0) + '</span></b>'
+                   + '<br>' + formatter.formatValue(size) + '</div>'        
+            }
+    }
+}
+
+
+var leto;
+var tip1; //Ali gre za prihodek ali odhodek
+var tip2; //Tip prihodka ali odhodka
+var podatki = []; //LineChart data
+var prihodki = []; //DonutChart data
+var odhodki = []; //DonutChart data
+var razdeljeno = {}; //TreeMap data
+var parent = {}; //gnezdenje za TreeMap
+
+document.getElementById('ponastaviButton').style.visibility = 'hidden';
+document.getElementById('ponastaviButton').onclick = function onClick (){
+    izrisTreemap();
+};
 //On page load
 getData()
     .then(data => {
